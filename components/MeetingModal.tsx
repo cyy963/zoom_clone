2b39d46b80cadd24b5ceecf6a906bbf28a5165c4
cyrus_w <cyrusw89@gmail.com>
2024-10-25 17:14:22 +1300

    add: components; Loader, MeetingModal

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -39,19 +39,32 @@ const MeetingModal = ({
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+        <DialogTitle />
         <div className="flex flex-col gap-6">
           {image && (
             <div>
               <Image src={image} alt="image" width={72} height={72} />
             </div>
           )}
+
           <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
             {title}
           </h1>
           {children}
-          <Button className="bg-blue-1 focus-visible:ring-offset-0" onClick={handleClick}>
-            {buttonIcon && (<Image src={buttonIcon} alt="button icon" width={13} height={13}/>)} &nbsp;
-            {buttonText || 'Schedule Meeting'}
+          <Button
+            className="bg-blue-1 focus-visible:ring-offset-0"
+            onClick={handleClick}
+          >
+            {buttonIcon && (
+              <Image
+                src={buttonIcon}
+                alt="button icon"
+                width={13}
+                height={13}
+              />
+            )}{" "}
+            &nbsp;
+            {buttonText || "Schedule Meeting"}
           </Button>
         </div>
       </DialogContent>
